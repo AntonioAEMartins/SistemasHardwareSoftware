23c0a7609dc92730dca4c9b590dc51d568df5373
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2023-04-13 17:23:21 -0300

    .

---
+++ material/aulas/14-processos/exemplo2-8filhos.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    pid_t pai, filho;
+    for (int i = 1; i < 9; i++){
+        filho = fork();
+        if (filho == 0){
+            printf("Eu sou o processo Filho %d", i);
+            exit();
+        }
+    }
+    pai = getpid();
+    printf("Eu sou o proceso pai %d", pai);
+    return 0;
+}
